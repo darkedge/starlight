0a46d15de59c4921dff03dc06c348cb4f1526708
Marco Jonkers <contact@marcojonkers.com>
2016-03-09 20:44:38 +0100

    Add own versions of _countof and ZeroMemory

--- starlight.h
+++ starlight.h
@@ -1,6 +1,30 @@
 #pragma once
+#include <string>
+
+#if 0
+int Stricmp(const char* str1, const char* str2) {
+	int d;
+	while ((d = toupper(*str2) - toupper(*str1)) == 0 && *str1) {
+		str1++;
+		str2++;
+	}
+	return d;
+}
+
+int Strnicmp(const char* str1, const char* str2, int count) {
+	int d = 0;
+	while (count > 0 && (d = toupper(*str2) - toupper(*str1)) == 0 && *str1) {
+		str1++; str2++; count--;
+	}
+	return d;
+}
+#endif
+
 #ifdef _MSC_VER
 #define SL_CALL __vectorcall
 #else
 #define SL_CALL
 #endif
+
+#define COUNT_OF(X) (sizeof(X) / sizeof((X)[0]))
+#define ZERO_MEM(X, Y) (memset(X, 0, Y));
