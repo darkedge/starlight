9afb00f4d0f08d34a242a41f559ffeb2a6448b3a
Marco Jonkers <contact@marcojonkers.com>
2016-04-02 00:39:23 +0200

    Update install.ps1

--- install.ps1
+++ install.ps1
@@ -1,16 +1,69 @@
+$shell = New-Object -com Shell.Application
+$webClient = New-Object Net.WebClient
+$externalDir = "external"
 
-$shell = new-object -com shell.application
-$zip = '7za920.zip'
-if (!(Test-Path .\7za920.zip)) {
-	Write-Host "Downloading 7-Zip..."
-	(New-Object Net.WebClient).DownloadFile('http://www.7-zip.org/a/7za920.zip','.\7za920.zip')
+$workingDir = $((Resolve-Path .).ToString() + "\")
+Write-Host $("Working directory: " + $workingDir)
+
+# Convenience functions
+function DownloadFile($address) {
+	$file = Split-Path $address -Leaf
+	$dest = $($workingDir + $file)
+	if ((Test-Path $dest)) {
+		Write-Host $("Found " + $file)
+		return
+	} else {
+		Write-Host $("Downloading " + $file + "...")
+		$webClient.DownloadFile($address, $dest)
+	}
 }
-$dir = $((Resolve-Path .).ToString() + "\")
-$join = Join-Path $dir $zip
-Write-Host "Extracting 7-Zip..."
-foreach ($item in $shell.namespace($join).Items()){
 
+function DownloadAndExtract($address) {
+	$file = Split-Path $address -Leaf
+	$dest = $($workingDir + $file)
+	if ((Test-Path $dest)) {
+		Write-Host $("Found " + $file)
+	} else {
+		Write-Host $("Downloading " + $file + "...")
+		$webClient.DownloadFile($address, $dest)
+	}
+
+	$extension = [System.IO.Path]::GetExtension($file)
+	Write-Host $("Extracting " + $file + "...")
+	if ($extension -eq ".zip") {
+		$cmd = ".\7za.exe x $file -o$externalDir -aos"
+        Write-Host $cmd
+		Invoke-Expression $cmd
+	} elseif ($extension -eq ".gz") { # .tar.gz
+        $cmd = ".\7za.exe e $file -aos"
+        Write-Host $cmd
+        Invoke-Expression $cmd
+
+        $tar = [System.IO.Path]::GetFileNameWithoutExtension($file)
+        $cmd = ".\7za.exe x $tar -o$externalDir -aos"
+        Write-Host $cmd
+        Invoke-Expression $cmd
+	} else {
+		Write-Host $($file + ": Unsupported archive format!")
+	}
+}
+
+DownloadFile("http://www.7-zip.org/a/7za920.zip")
+
+Write-Host Extracting 7-Zip executable...
+foreach ($item in $shell.NameSpace($($workingDir + "7za920.zip")).Items()) {
     if ($item.Name -eq "7za.exe") {
-        $shell.Namespace($dir).copyhere($item)
+        $shell.NameSpace($workingDir).CopyHere($item, 16)
     }
 }
+
+DownloadAndExtract("http://enet.bespin.org/download/enet-1.3.13.tar.gz")
+DownloadAndExtract("https://github.com/g-truc/glm/releases/download/0.9.7.2/glm-0.9.7.2.zip")
+DownloadAndExtract("https://github.com/ocornut/imgui/archive/v1.47.zip")
+DownloadAndExtract("http://www.lua.org/ftp/lua-5.1.5.tar.gz")
+DownloadAndExtract("http://cppcms.com/files/nowide/nowide_standalone.zip")
+DownloadAndExtract("https://github.com/google/protobuf/releases/download/v2.6.1/protobuf-2.6.1.zip")
+DownloadAndExtract("https://github.com/google/flatbuffers/archive/v1.3.0.zip")
+
+# ...missing flatbuffers!
+
