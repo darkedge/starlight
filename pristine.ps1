2b5bbcc3d56072ec0bd13aa2f78bca59b32ab0da
Marco Jonkers <contact@marcojonkers.com>
2016-04-21 08:59:35 +0200

    pristine.ps1 now finds Git first

--- pristine.ps1
+++ pristine.ps1
@@ -1,3 +1,11 @@
 .\clean.ps1
+
+# Add git to path
+if ((Test-Path $($env:ProgramFiles + "\Git\bin"))) {
+    $env:Path += $(";" + $env:ProgramFiles + "\Git\bin")
+} elseif ((Test-Path $(${env:ProgramFiles(x86)} + "\Git\bin"))) {
+    $env:Path += $(";" + ${env:ProgramFiles(x86)} + "\Git\bin")
+}
+
 $cmd = "git clean -d -f -f -X"
 Invoke-Expression $cmd
