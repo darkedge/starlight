f725adeacbaf61fd29597641086e53fe6edd3819
Marco Jonkers <contact@marcojonkers.com>
2016-05-28 12:12:17 +0200

    Fix unity build

--- src/starlight_win32_ub.cpp
+++ src/starlight_win32_ub.cpp
@@ -1,4 +1,5 @@
 #define SL_UB
+// starlight_win32.cpp defines SL_IMPL so include that first
+#include "starlight_win32.cpp"
 #include "starlight_d3d11.cpp"
 #include "starlight_d3d11_imgui.cpp"
-#include "starlight_win32.cpp"
