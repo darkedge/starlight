2f4d76a8930276dc0ed0237776c0f53dc89abcec
Marco Jonkers <contact@marcojonkers.com>
2016-06-12 04:34:58 +0200

    Revert "Add useless annotations for WinMain"
    This reverts commit 5e7c996589f3e073bc219d97e681800a7b9f2b12.

--- src/starlight_win32.cpp
+++ src/starlight_win32.cpp
@@ -323,10 +323,10 @@ LRESULT WINAPI WndProc(HWND hWnd, UINT msg, WPARAM wParam, LPARAM lParam) {
 }
 
 int CALLBACK WinMain(
-	_In_ HINSTANCE   hInstance,
-	_In_opt_ HINSTANCE   hPrevInstance,
-	_In_ LPSTR       lpCmdLine,
-	_In_ int         nCmdShow)
+	HINSTANCE   hInstance,
+	HINSTANCE   hPrevInstance,
+	LPSTR       lpCmdLine,
+	int         nCmdShow)
 {
 	//std::set_new_handler(memory::no_memory);
 
