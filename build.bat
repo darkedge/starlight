b4f378f136e8c0c6338ff162766bd4c6f0b18bdc
Marco Jonkers <contact@marcojonkers.com>
2016-07-15 01:02:45 +0200

    Batch file tweaks

--- build.bat
+++ build.bat
@@ -1,1 +1,4 @@
-@jom /j %NUMBER_OF_PROCESSORS% /nologo "SL_RANDOM=%RANDOM%"
+@echo off
+pushd %~dp0
+jom /j %NUMBER_OF_PROCESSORS% /nologo "SL_RANDOM=%RANDOM%"
+popd
--- make.bat
+++ make.bat
@@ -1,4 +1,5 @@
 @echo off
 pushd %~dp0
-jom /j %NUMBER_OF_PROCESSORS% "SL_RANDOM=%RANDOM%"
+where cl.exe || call "%VS140COMNTOOLS:~0,-14%VC\vcvarsall.bat" amd64
+jom /j %NUMBER_OF_PROCESSORS% /nologo "SL_RANDOM=%RANDOM%"
 popd
